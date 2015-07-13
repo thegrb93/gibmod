6480bc43e714ed3e16e8681fbd163b8ec74542d6
Garrett Brown <grbrown93@sbcglobal.net>
2015-07-12 23:33:18 -0500

    Keep players from spamming the splat sound effects when dragging flesh across the ground.

--- lua/entities/gib_chunk/init.lua
+++ lua/entities/gib_chunk/init.lua
@@ -13,18 +13,21 @@ function ENT:Initialize()
 end
  	
 function ENT:PhysicsCollide( data, physobj )	
-	-- sound
-	math.randomseed( math.random() )
-	local rand = math.random(1,5)
-	local sound = "physics/flesh/flesh_squishy_impact_hard1.wav"
-	
-	if rand >= 1 and rand <= 4 then
-		sound = "physics/flesh/flesh_squishy_impact_hard"..math.random(1,4)..".wav"
-	elseif rand == 5 then
-		sound = "physics/flesh/flesh_bloody_break.wav"
+	if not self.Sounded then
+		self.Sounded = true
+		-- sound
+		math.randomseed( math.random() )
+		local rand = math.random(1,5)
+		local sound = "physics/flesh/flesh_squishy_impact_hard1.wav"
+		
+		if rand >= 1 and rand <= 4 then
+			sound = "physics/flesh/flesh_squishy_impact_hard"..math.random(1,4)..".wav"
+		elseif rand == 5 then
+			sound = "physics/flesh/flesh_bloody_break.wav"
+		end
+		
+		self:EmitSound( sound, 50, math.Clamp( math.Clamp( (self:BoundingRadius() * 10), 1, 5000 ) * -1 + 255 + math.random(-5, 5), 50, 255 )  )
 	end
-	
-	self:EmitSound( sound, 100, math.Clamp( math.Clamp( (self:BoundingRadius() * 10), 1, 5000 ) * -1 + 255 + math.random(-5, 5), 50, 255 )  )
 	
 	-- blood spurt
 	local effectdata = EffectData()
