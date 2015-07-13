d524afb449ec5148089bbe61377f2567afdc8366
Garrett Brown <grbrown93@sbcglobal.net>
2015-07-12 23:33:18 -0500

    Keep players from spamming the splat sound effects when dragging flesh across the ground.

--- lua/entities/gib_chunk/init.lua
+++ lua/entities/gib_chunk/init.lua
@@ -3,6 +3,8 @@ AddCSLuaFile( "shared.lua" )
 
 include('shared.lua')
 
+--Maximum splats. 0 for infinite.
+local splatLimit = CreateConVar( "gibmod_maxsplatsounds", "0", { FCVAR_ARCHIVE, FCVAR_DEMO, FCVAR_REPLICATED } )
 
 function ENT:Initialize()		
     self:PhysicsInit( SOLID_VPHYSICS )
@@ -10,21 +12,25 @@ function ENT:Initialize()
     self:SetSolid( SOLID_VPHYSICS )
 	
 	self:SetCollisionGroup( COLLISION_GROUP_DEBRIS )
+	self.SplatSounds = 0
 end
  	
 function ENT:PhysicsCollide( data, physobj )	
-	-- sound
-	math.randomseed( math.random() )
-	local rand = math.random(1,5)
-	local sound = "physics/flesh/flesh_squishy_impact_hard1.wav"
-	
-	if rand >= 1 and rand <= 4 then
-		sound = "physics/flesh/flesh_squishy_impact_hard"..math.random(1,4)..".wav"
-	elseif rand == 5 then
-		sound = "physics/flesh/flesh_bloody_break.wav"
+	if splatLimit:GetInt()==0 or self.SplatSounds < splatLimit:GetInt() then
+		self.SplatSounds = self.SplatSounds + 1
+		-- sound
+		math.randomseed( math.random() )
+		local rand = math.random(1,5)
+		local sound = "physics/flesh/flesh_squishy_impact_hard1.wav"
+		
+		if rand >= 1 and rand <= 4 then
+			sound = "physics/flesh/flesh_squishy_impact_hard"..math.random(1,4)..".wav"
+		elseif rand == 5 then
+			sound = "physics/flesh/flesh_bloody_break.wav"
+		end
+		
+		self:EmitSound( sound, 50, math.Clamp( math.Clamp( (self:BoundingRadius() * 10), 1, 5000 ) * -1 + 255 + math.random(-5, 5), 50, 255 )  )
 	end
-	
-	self:EmitSound( sound, 100, math.Clamp( math.Clamp( (self:BoundingRadius() * 10), 1, 5000 ) * -1 + 255 + math.random(-5, 5), 50, 255 )  )
 	
 	-- blood spurt
 	local effectdata = EffectData()
