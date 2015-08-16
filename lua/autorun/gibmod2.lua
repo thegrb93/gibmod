802acc1bc35321bd54ebf0a57c8e3d646ace44f9
Garrett Brown <grbrown93@sbcglobal.net>
2015-08-15 20:29:55 -0500

    Use pelvis position instead of glitchy GetPos

--- lua/autorun/gibmod2.lua
+++ lua/autorun/gibmod2.lua
@@ -359,7 +359,7 @@ function GibMod_Explode( ent, damageForce, isExplosionDamage )
 	
 	ent.GibMod_Exploded = true
 	
-	local pos = ent:GetPos()
+	local pos = ent:GetBonePosition( ent:LookupBone( "ValveBiped.Bip01_Pelvis" ) or 0 )
 	local vel = ent:GetVelocity()
 	
 	-- go down
